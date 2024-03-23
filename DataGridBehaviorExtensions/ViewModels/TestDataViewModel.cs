a7fe29e2b235524d91eb9a9abbe1426b1ecdd048
Дмитрий Евреинов <d.evreinov@outlook.com>
2024-03-23 19:09:56 +0300

    feat: :sparkles: добавлено поведение для элемента DataGrid.
    При автоматической генерации столбцов, в заголовок столбца вместо наименования свойства, записывается значение атрибута DisplayName.

--- DataGridBehaviorExtensions/ViewModels/TestDataViewModel.cs
+++ DataGridBehaviorExtensions/ViewModels/TestDataViewModel.cs
@@ -1,17 +1,23 @@
-﻿using DataGridBehaviorExtensions.ViewModels.Base;
+﻿using System.ComponentModel;
+using DataGridBehaviorExtensions.ViewModels.Base;
 
 namespace DataGridBehaviorExtensions.ViewModels
 {
     internal class TestDataViewModel : ViewModel
     {
+        [DisplayName("Идентификатор")]
         public int Id { get; set; }
 
+        [DisplayName("Наименование")]
         public string Name { get; set; } = string.Empty;
 
+        [DisplayName("Описание")]
         public string Description { get; set; } = string.Empty;
 
+        [DisplayName("Тип")]
         public string Type { get; set; } = string.Empty;
 
+        [DisplayName("Описание типа")]
         public string TypeDescription { get; set; } = string.Empty;
     }
 }
